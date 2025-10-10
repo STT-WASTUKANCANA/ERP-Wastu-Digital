970c3a111685095a54ece864908bf1d82e706929
ifanprima130206 <fanproject13@gmail.com>
2025-10-10 11:09:29 +0700

    feat(summary-card): Integrate API data for summary counts

--- frontend/src/app/workspace/overview/page.tsx
+++ frontend/src/app/workspace/overview/page.tsx
@@ -1,33 +1,56 @@
-import { WorkSpaceLayout } from '@/components/layouts/workspace-layout'
-import { Card } from '@/components/ui/card'
-import React from 'react'
-import { BsInbox, BsSend } from 'react-icons/bs'
-import { LuUsers } from 'react-icons/lu'
+import { WorkSpaceLayout } from "@/components/layouts/workspace-layout";
+import { Card } from "@/components/ui/card";
+import { getIncomingMailSummary } from "@/lib/api/mails/incoming";
+import React from "react";
+import { BsInbox, BsSend } from "react-icons/bs";
+import { LuUsers } from "react-icons/lu";
 
-const page = () => {
+const Page = async () => {
+  const response = await getIncomingMailSummary();
+
+  if (!response.ok) {
+    return (
+      <WorkSpaceLayout>
+        <p className="text-red-500">Gagal memuat atau format data ringkasan tidak valid.</p>
+      </WorkSpaceLayout>
+    );
+  }
+
+  const incomingMailData = response.data.data;
+
+  const incomingMailValue = incomingMailData.current_month_total;
+
+  let incomingMailPercent = incomingMailData.percentage_change;
+  if (incomingMailData.status !== 'increase') {
+    incomingMailPercent = -incomingMailPercent;
+  }
+
+  const outgoingMailValue = "...";
+  const usersValue = "..."; 
+
   return (
     <WorkSpaceLayout>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
         <Card
           title="Incoming Mail"
-          value="1.250"
-          percent={-12.5}
+          value={incomingMailValue.toString()}
+          percent={incomingMailPercent}
           icon={BsInbox}
         />
         <Card
           title="Outgoing Mail"
-          value="1.250"
-          percent={12.5}
+          value={122}
+          percent={0}
           icon={BsSend}
         />
         <Card
           title="Users"
-          value="20"
+          value={0}
           icon={LuUsers}
         />
       </div>
     </WorkSpaceLayout>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
---
+++ frontend/src/lib/api/mails/incoming.ts
@@ -0,0 +1,34 @@
+import { cookies } from "next/headers";
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export async function getIncomingMailSummary() {
+        if (!API_URL) {
+                throw new Error("API URL is not defined");
+        }
+
+        try {
+                const cookieStore = cookies();
+                const token = cookieStore.get('access_token')?.value;
+
+                const res = await fetch(`${API_URL}/mails/incoming/summary`, {
+                        method: "GET",
+                        headers: {
+                                Accept: "application/json",
+                                'Authorization': `Bearer ${token}`,
+                        },
+                });
+
+                const data = await res.json();
+
+                // console.log("Response OK?:", res.ok);
+                // console.log("Response Status:", res.status);
+                // console.log("Response Data:", JSON.stringify(data, null, 2));
+
+                return { ok: res.ok, data };
+
+        } catch (error) {
+                console.error("Fetch failed:", error);
+                return { ok: false, data: null, error: error };
+        }
+}
